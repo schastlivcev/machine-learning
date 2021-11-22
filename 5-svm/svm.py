d080d87fa6dd68c066b9b64ba6bb15546c77448d
Mikhail Schastlivcev <mikhail.schastlivcev@waveaccess.ru>
2021-11-22 13:48:46 +0300

    update svm

--- 5-svm/svm.py
+++ 5-svm/svm.py
@@ -11,16 +11,16 @@ class SVM:
         self.model.fit(x_train, y_train)
 
     def calculate_lines(self):
-        w = self.model.coef_[0]  # w consists of 2 elements
-        b = self.model.intercept_[0]  # b consists of 1 element
+        w = self.model.coef_[0]
+        b = self.model.intercept_[0]
         x_points = np.linspace(0, 1024)  # generating x-points from -1 to 1
-        y_points = -(w[0] / w[1]) * x_points - b / w[1]  # getting corresponding y-points
+        y_points = -(w[0] / w[1]) * x_points - b / w[1]
         main_line_arr = []
         for x, y in zip(x_points, y_points):
             main_line_arr.append([x, y])
 
-        w_hat = self.model.coef_[0] / (np.sqrt(np.sum(self.model.coef_[0] ** 2)))  # Step 3 (margin):
-        margin = 1 / np.sqrt(np.sum(self.model.coef_[0] ** 2))  # Step 4 (calculate points of the margin lines):
+        w_hat = self.model.coef_[0] / (np.sqrt(np.sum(self.model.coef_[0] ** 2)))  # margin:
+        margin = 1 / np.sqrt(np.sum(self.model.coef_[0] ** 2))  # calculate points of the margin lines:
         decision_boundary_points = np.array(list(zip(x_points, y_points)))
         points_of_line_above = decision_boundary_points + w_hat * margin
         points_of_line_below = decision_boundary_points - w_hat * margin
